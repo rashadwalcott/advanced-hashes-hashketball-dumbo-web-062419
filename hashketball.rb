b6f2d041eb9a6ecdf1d540fe6f0e73ba22557dc0
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 19:56:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,8 +213,7 @@ end
   home = 0
     game_hash.each do |team_pos,team_stats|
       binding.pry
-    team_pos[:home].each do |player_name, player_stats|
-       
+    if team_pos[:home]
       player_stats.each do |key,value|
         if key == :points
     home = home + value
@@ -223,7 +222,7 @@ end
 end
 end
   game_hash.each do |team_pos,team_stats|
-    team_pos[:away].each do |player_name, player_stats|
+    if team_pos[:away]
     player_stats.each do |key, value|
       if key == :points
     away = away + value
