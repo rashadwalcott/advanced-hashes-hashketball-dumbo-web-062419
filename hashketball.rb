a44537ce10349e7e11746da12bf9695119dd8419
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 06:03:40 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,49 +1,153 @@
 # Write your code here!
 def game_hash 
-  game = {:home => {
-    :team_name => {"Brooklyn Nets"},
-    :colors => {"Black","White"},
-    :players => [{
-      :number => {0},{30},{11},{1},{31},
-      :shoe => {16,14,17,19,15},
-      :points => {22,12,17,26,19},
-      :rebounds => {12,12,19,11,2},
-      :assists => {12,12,10,6,2},
-      :steals => {3,12,3,3,4},
-      :blocks => {1,12,1,8,11},
-      :slam_dunks => {1,7,15,5,1}
-      
-      }]
-    
+  hash = {:home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => {
+      "Alan Anderson" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      },
+      "Reggie Evans" => {
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      },
+      "Brooke Lopez" => {
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
+      },
+      "Mason Plumlee" => {
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
+      },
+      "Jason Terry" => {
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
+      }
+    }
   },
-  :away => {
-    :team_name => {},
-    :colors => {},
-    :players => [{
-      :number => {},
-      :shoe => {},
-      :points => {},
-      :rebounds => {},
-      :assists => {},
-      :steals => {},
-      :blocks => {},
-      :slam_dunks => {}
-      
-      }]
-      
-      
-    
-  }
-    
-  }
+  :away => {:team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => {
+      "Jeff Adrien" => {
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+      },
+      "Bismak Biyombo" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      },
+      "DeSagna Diop" => {
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+      },
+      "Ben Gordon" => {
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
+      },
+      "Kemba Walker" => {
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12
+      }
+     }
+    }
+   }
   
 end
 
+def num_points_scored(name)
+  array= []
+  game_hash.each do |team_pos,team_stats|
+    team_stats[:players].each do |player_name,player_stats|
+      if player_name == name
+        array << player_stats[:points]
+      end
+    end
+  end
+  array
+end
 
+def shoe_size (name)
+  array =[]
+  game_hash.each do |team_pos,team_stats|
+    team_stats[:players].each do |player_name,player_stats|
+      if player_name.include? name
+        array << player_stats[:shoe]
+      end
+    end
+  end
+  array
+end
 
-
-
-
+def team_colors(name)
+ array =[]
+  game_hash.each do |team_pos,team_stats|
+      if team_stats[:team_name] == name
+        array << team_stats[:colors]
+      end
+    end
+  array
+end
 
 
 
